f1291330a46b8190e74edd699e1378b917702954
yipmunallen <ymallen@hotmail.co.uk>
2021-01-04 21:59:00 +0000

    creating settings to set api url

--- assets/javascript/settings.js
+++ assets/javascript/settings.js
@@ -1,17 +1,44 @@
-class Settings{
-    constructor(){
-        this.category = document.getElementById(`category`);
-        this.difficulty = [
-            document.getElementById(`easy`),
-            document.getElementById(`medium`),
-            document.getElementById(`hard`),
-        ]
-        this.numberofQuestions = document.getElementById(`questions`);
-    }
-    startQuiz(){
 
+
+class Settings {
+  constructor() {
+    this.category = document.querySelector('#category');
+    this.difficulty = [
+      document.querySelector('#easy'),
+      document.querySelector('#medium'),
+      document.querySelector('#hard'),
+    ];
+    this.startButton = document.querySelector('#quiz-btn');
+    this.startButton.addEventListener('click', this.startQuiz.bind(this));
+  }
+
+  startQuiz() {
+    try{
+      const categoryId = this.category.value;
+      console.log(categoryId + "Settings page");
+      const difficulty = this.getDifficulty();
+      this.goToQuiz();
+
+    }catch(error){
+      alert(error);
     }
+  }
+
+  goToQuiz() {
+    window.location.href="/quiz.html";
+  }
+
+  getDifficulty() {
+    const chosenDifficulty = this.difficulty.filter(element => element.checked);
+
+    if (chosenDifficulty.length === 1) {
+      return chosenDifficulty[0].id;
+    } else {
+      throw new Error('Please select a difficulty!');
+    }
+  }
 }
 
+new Settings();
 
-export default Settings;
+export {categoryID, difficulty };
