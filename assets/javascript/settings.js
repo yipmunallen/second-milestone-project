dedb8c23c99fe10395480dfbe94e432cbf86e3dd
yipmunallen <ymallen@hotmail.co.uk>
2021-01-05 13:36:32 +0000

    Removing class constructor

--- assets/javascript/settings.js
+++ assets/javascript/settings.js
@@ -1,44 +1,36 @@
-
-
-class Settings {
-  constructor() {
-    this.category = document.querySelector('#category');
-    this.difficulty = [
+const category = document.querySelector('#category');
+const difficulty = [
       document.querySelector('#easy'),
       document.querySelector('#medium'),
       document.querySelector('#hard'),
     ];
-    this.startButton = document.querySelector('#quiz-btn');
-    this.startButton.addEventListener('click', this.startQuiz.bind(this));
-  }
+const startButton = document.querySelector('#quiz-btn');
 
-  startQuiz() {
+startButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
     try{
-      const categoryId = this.category.value;
+      var categoryId = category.value;
       console.log(categoryId + "Settings page");
-      const difficulty = this.getDifficulty();
-      this.goToQuiz();
+      var difficulty = getDifficulty();
+      goToQuiz();
 
     }catch(error){
       alert(error);
     }
   }
 
-  goToQuiz() {
+function  goToQuiz() {
     window.location.href="/quiz.html";
   }
 
-  getDifficulty() {
-    const chosenDifficulty = this.difficulty.filter(element => element.checked);
+function  getDifficulty() {
+    var chosenDifficulty = difficulty.filter(element => element.checked);
 
     if (chosenDifficulty.length === 1) {
       return chosenDifficulty[0].id;
     } else {
       throw new Error('Please select a difficulty!');
     }
-  }
 }
 
-new Settings();
-
-export {categoryID, difficulty };
