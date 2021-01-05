3b0ba9c7c14af0dbc1e53d57f36485b948bd681b
yipmunallen <ymallen@hotmail.co.uk>
2021-01-05 18:49:50 +0000

    Adding functionality for "Next Question" button

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -3,16 +3,18 @@ const progressBar = document.getElementById(`progress-bar`);
 const progressBarFull = document.getElementById("progress-bar-full");
 const timeCount = document.getElementById("timer-count");
 const question = document.getElementById(`api-question`);
+const nextQuestionBtn = document.getElementById(`next-question-btn`);
 const answers = Array.from(document.getElementsByClassName(`answers`));
 
 let currentQuestion = {};
 let questionCounter = 1;
 let startTime = 10;
-let acceptingAnswers = false;
 let questionList = [];
 let score = 0;
 let questionIndex = 0;
 let counter;
+let outOfTime = false;
+let acceptingAnswers;
 
 function getQuizData(quizData){
     var xhr = new XMLHttpRequest();
@@ -25,34 +27,31 @@ function getQuizData(quizData){
 
     xhr.open( "GET" , "https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple");
     xhr.send();
-
 }
 
-nextQuestion();
+getQuizData(function(quizData){
 
-function nextQuestion(){
+    nextQuestion();
 
-    getQuizData(function(quizData){
+    function nextQuestion(){ 
+  
+        if (questionIndex === 20) {
+            localStorage.setItem('finalScore', score);
+            //go to the end page
+            return window.location.href = '/score.html' ;
+        }
 
+        acceptingAnswers = true;
+        nextQuestionBtn.style.visibility = 'hidden';
         timeCount.style.color = null;
         startTimer(startTime);
-        
-        if (questionIndex === 20) {
-            localStorage.setItem('mostRecentScore', score);
-            //go to the end page
-            window.location.href('/end-page.html');
-        }
 
         progressHeading.innerText = `Question ${questionCounter} of 20`;
         progressBarFull.style.width = `${(questionCounter / 20) * 100}%`;
-
         currentQuestion = quizData.results[questionIndex];
-
         question.innerHTML = `${currentQuestion.question}`;
-
         correctAnswer = answers[Math.floor(Math.random()*answers.length)];
         correctAnswer.innerHTML = `${currentQuestion.correct_answer}`;
-        
         for( i=j=0 ; j<3 && i < 4 ; i++){
             if(answers[i] == correctAnswer){
             }
@@ -61,59 +60,74 @@ function nextQuestion(){
                 j++;
             }
         };
-    })
-}
+        }
 
-function startTimer(time){
-    counter = setInterval(timer, 1000);
-    function timer(){
-        timeCount.innerText = time;
-        time--;
-        if(time < 5){
-            timeCount.style.color = "red";
+    // Timer starts at 10 once nextQuestion() runs
+    function startTimer(time){
+        counter = setInterval(timer, 1000);
+        function timer(){
+            timeCount.innerText = time;
+            time--;
+            // Once timer is under 5, it's color turns red
+            if(time < 5){
+                timeCount.style.color = "red";
+            }
+            // Once timer is below 0, show "Out Of Time" , stop accepting answers and show "Next Question" button
+            if(time < 0){
+                clearInterval(counter);
+                timeCount.innerText = "Out of Time!";
+                questionIndex++;
+                questionCounter++;
+                correctAnswer.classList.add("correct-answer");
+                nextQuestionBtn.style.visibility = 'visible';
+                acceptingAnswers = false;
+                nextQuestionBtn.addEventListener("click", goToNextQuestion);
+                    function goToNextQuestion(e) {
+
+                    correctAnswer.classList.remove("correct-answer");
+                    timeCount.style.color = null;
+                    nextQuestion();        
+                }
+            }
         }
-        if(time < 0){
-            clearInterval(counter);
-            timeCount.innerText = "Out of Time!";
-            questionIndex++;
-            questionCounter++;
-            correctAnswer.classList.add("correct-answer");
-            setTimeout(() => {
-                correctAnswer.classList.remove("correct-answer");
-                timeCount.style.color = null;
-                nextQuestion();
-            }, 1000);
-        }
+
     }
 
-}
+    // Determines what happens when a correct/incorrect answer is clicked
 
-// Determine what happens when a correct/incorrect answer is clicked
+    answers.forEach(function(answer) {
+        answer.addEventListener("click", clickFunction);
+        function clickFunction(e) {
+                if (!acceptingAnswers) return;
+                // Stop timer 
+                clearInterval(counter);
+                // Stop accepting answers
+                acceptingAnswers = false;
 
-answers.forEach(function(answer) {
-answer.addEventListener("click", clickFunction);
-function clickFunction(e) {
+                const selectedAnswer = e.target;
 
-        const selectedAnswer = e.target;
+                // If correct answer has been selected, turn div green and increase score
+                if(selectedAnswer == correctAnswer){
+                        selectedAnswer.classList.add("correct-answer");
+                        score++;
+                        console.log(score);
+                }
+                // If incorrect answer has been selected, turn div red
+                else{
+                        selectedAnswer.classList.add("incorrect-answer");
+                }
 
-        if(selectedAnswer == correctAnswer){
-            selectedAnswer.classList.add("correct-answer");
-            score++;
+                questionIndex++;
+                questionCounter++;
+                nextQuestionBtn.style.visibility = 'visible';
+                acceptingAnswers = false;
+                nextQuestionBtn.addEventListener("click", goToNextQuestion);
+                    function goToNextQuestion(e) {
+                    selectedAnswer.classList.remove("incorrect-answer");
+                    selectedAnswer.classList.remove("correct-answer");
+                    clearInterval(counter);
+                    nextQuestion();    
+                }
         }
-        else{
-            selectedAnswer.classList.add("incorrect-answer");
-        }
-
-        questionIndex++;
-        questionCounter++;
-        setTimeout(() => {
-            selectedAnswer.classList.remove("incorrect-answer");
-            selectedAnswer.classList.remove("correct-answer");
-            clearInterval(counter);
-
-            nextQuestion();
-        }, 1000);
-
-    
-}
-});
+    });
+})
