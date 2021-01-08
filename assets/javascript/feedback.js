c83114d1d09635e244b1ba9e5d31933e36f9b8d8
yipmunallen <ymallen@hotmail.co.uk>
2021-01-08 11:30:27 +0000

    Adding alerting when success/error

--- assets/javascript/feedback.js
+++ assets/javascript/feedback.js
@@ -7,10 +7,16 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert("Thank you for your feedback!");
+            document.getElementById('fullname').value=''; 
+            document.getElementById('emailaddress').value=''; 
+            document.getElementById('feedbacksummary').value='';
         },
         function(error) {
             console.log("FAILED", error);
+            alert("An error has occured, please try again later");
         }
     );
+ 
     return false;  // To block from loading a new page
 }
